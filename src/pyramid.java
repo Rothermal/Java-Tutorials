45efc2d749b8797d3400469a17bc7ac952b1336d
Mark Rothermal <rothermal2@comcast.net>
2016-10-04 17:04:49 -0500

    some more tutorials

--- src/pyramid.java
+++ src/pyramid.java
@@ -1,12 +1,12 @@
-
-public class pyramid {
-	
-	public static void main (String args[]) {
-		for (int x = 1; x < 10; x++){
-			for(int y = 1; y < x; y++){
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-	}
-}
+//
+//public class pyramid {
+//	
+//	public static void main (String args[]) {
+//		for (int x = 1; x < 10; x++){
+//			for(int y = 1; y < x; y++){
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
---
+++ src/pyramidReverse.java
@@ -0,0 +1,11 @@
+//
+//public class pyramidReverse {
+//	public static void main(String args[]){
+//		for(int x=10; x > 1; x--){
+//			for(int y = 1 ;y < x;y++){
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+//	}
+//}
