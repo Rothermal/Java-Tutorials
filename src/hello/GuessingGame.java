27aa308524f2cf8a2daa9eacb20e1c13126b841e
Mark Rothermal <rothermal2@comcast.net>
2016-10-06 17:35:02 -0500

    started numbers guessing game

---
+++ src/hello/GuessingGame.java
@@ -0,0 +1,17 @@
+package hello;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessingGame {
+	public static void main(String args[]){
+		
+		Random rand = new Random();
+		int numberToGuess = rand.nextInt(1000);
+		int numberOfTries = 0;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Guess a number between 1 and 1000: ");
+		
+	}
+}
---
+++ src/hello/conditionals.java
@@ -0,0 +1,20 @@
+//package hello;
+//
+//import java.util.Scanner;
+//
+//
+//
+//public class conditionals {
+//	public static void main(String args[]){
+//		Scanner scan = new Scanner(System.in);
+//		int num = scan.nextInt();
+//		if(num>6){
+//			if (num > 8){
+//				System.out.println("The Number is greater than 8");
+//			}
+//			else{
+//				System.out.println("The Number is greater than 6 but less than 9");
+//			}
+//		}
+//	}
+//}
