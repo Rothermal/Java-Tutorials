154bef9f677fc2c754a20bf44f04055a5a26d79a
Mark Rothermal <rothermal2@comcast.net>
2016-10-08 14:15:38 -0500

    start of 21 sticks game

--- src/hello/MainExample.java
+++ src/hello/MainExample.java
@@ -1,28 +1,28 @@
-package hello;
-
-public class MainExample {
-
-	public static void main(String args []){
-		System.out.println("Method Example");
-		sayHello();
-//		int num = five();
-//		System.out.println(num);
-		counter();
-	}
-	public static void sayHello(){
-		System.out.println("Hello");
-	}
-	public static int five(){
-		int num = 5;
-		return num;
-	}
-	public static void counter(){
-		System.out.println("Count to five, Five times");
-		for(int y = 0; y<5; y++){
-			System.out.println("Time number "+ (y+1));
-			for(int i = 0; i<5; i++){
-				System.out.println(i+1);
-			}
-		}
-	}
-}
+//package hello;
+//
+//public class MainExample {
+//
+//	public static void main(String args []){
+//		System.out.println("Method Example");
+//		sayHello();
+////		int num = five();
+////		System.out.println(num);
+//		counter();
+//	}
+//	public static void sayHello(){
+//		System.out.println("Hello");
+//	}
+//	public static int five(){
+//		int num = 5;
+//		return num;
+//	}
+//	public static void counter(){
+//		System.out.println("Count to five, Five times");
+//		for(int y = 0; y<5; y++){
+//			System.out.println("Time number "+ (y+1));
+//			for(int i = 0; i<5; i++){
+//				System.out.println(i+1);
+//			}
+//		}
+//	}
+//}
---
+++ src/hello/TwentyOneSticks.java
@@ -0,0 +1,16 @@
+package hello;
+
+import java.util.Scanner;
+
+public class TwentyOneSticks {
+	
+	public static void main(String args []){
+		int sticks = 21;
+		int playerOneTotal;
+		int playerTwoTotal;
+		Scanner input = new Scanner(System.in);
+		Scanner numOfSticks = new Scanner(System.in);
+		System.out.println("Would you like to go first? Y/N");
+		String goFirst = input.nextLine();
+	}
+}
