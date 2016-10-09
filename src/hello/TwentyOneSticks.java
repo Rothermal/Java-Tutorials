c544b0e9d38f55e469671a74b6e149fbfa4b6d70
Mark Rothermal <rothermal2@comcast.net>
2016-10-09 16:12:38 -0500

    21 sticks complete

--- src/hello/TwentyOneSticks.java
+++ src/hello/TwentyOneSticks.java
@@ -8,6 +8,7 @@ public class TwentyOneSticks {
 		int sticks = 21;
 		int player;
 		int computer;
+		int pickup;
 		Scanner input = new Scanner(System.in);
 		Scanner numOfSticks = new Scanner(System.in);
 		System.out.println("Would you like to go first? Y/N");
@@ -16,30 +17,36 @@ public class TwentyOneSticks {
 		while(sticks > 0){
 			char result = goFirst.charAt(0);
 			if(result == 'y' || result == 'Y'){
-				System.out.println("Player has decided to pick first");
+				System.out.println("It Is Your Turn");
 				System.out.println("There are "+sticks+" left");
 				System.out.println("How many sticks would you like to take ( 1 or 2 )");
-				int pickup = numOfSticks.nextInt();
+			    pickup = numOfSticks.nextInt();
 				if (pickup > 2){
 					pickup = 2;
 				}
-				if (pickup < 2){
+				else if (pickup < 2){
 					pickup = 1;
 				}
-				sticks = sticks - pickup;
+				
+				sticks = (sticks - pickup);
+				System.out.println("There are "+sticks+" left");
 				if(sticks <= 0){
 					System.out.println("YOu LOSE!!!!");
 				}
 				else{
-					if(sticks - 2 % 3 == 0 || sticks -2 == 0 ){
+					if((sticks - 2) % 3 == 0 || sticks -2 == 0 ){
 						pickup = 1;
 					}
 					else {
-						pickup = -2;
+						pickup = 2;
 					}
+					
 					System.out.println("The Computer takes "+pickup+" sticks!");
-					sticks = sticks - pickup;
+					
+					sticks = (sticks - pickup);
+					
 					System.out.println("There are "+sticks+" sticks left");
+					
 					if(sticks <= 0){
 						System.out.println("You Win!!!");
 					}
@@ -47,7 +54,42 @@ public class TwentyOneSticks {
 				}
 			}
 			else{
-				System.out.println("The Computer shall go first");
+				
+				System.out.println("It is the Computer's turn");
+				if((sticks - 2) % 3 == 0 || sticks -2 == 0 ){
+					pickup = 1;
+				}
+				else {
+					pickup = 2;
+				}
+				
+				System.out.println("The Computer takes "+pickup+" sticks!");
+				
+				sticks = (sticks - pickup);
+				
+				System.out.println("There are "+sticks+" sticks left");
+				
+				if(sticks <= 0){
+					System.out.println("You Win!!!");
+				}
+				else  {
+					System.out.println("It Is Your Turn");
+					System.out.println("There are "+sticks+" left");
+					System.out.println("How many sticks would you like to take ( 1 or 2 )");
+				    pickup = numOfSticks.nextInt();
+					if (pickup > 2){
+						pickup = 2;
+					}
+					else if (pickup < 2){
+						pickup = 1;
+					}
+					
+					sticks = (sticks - pickup);
+					System.out.println("There are "+sticks+" left");
+					if(sticks <= 0){
+						System.out.println("YOu LOSE!!!!");
+					}
+				}
 
 			}
 		}
