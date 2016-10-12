4a66476a438eafc8e4ccd6bd0e494d429ba1a04b
Mark Rothermal <rothermal2@comcast.net>
2016-10-11 21:48:37 -0500

    working with methods

---
+++ src/hello/MethodsPartDeux.java
@@ -0,0 +1,23 @@
+package hello;
+
+import java.util.Scanner;
+
+public class MethodsPartDeux {
+
+	public static void main(String args []){
+		
+
+	}
+	 public static String methodOne(){
+		String a = "Hello";
+		return a;
+	}
+	 public static void methodTwo(String greeting){
+		 String b = "How are you today";
+		 String c = greeting + " " + b;
+		  
+	 }
+	 public static String methodThree(String greeting){
+		 Scanner name = new Scanner(System.in);
+	 }
+}
