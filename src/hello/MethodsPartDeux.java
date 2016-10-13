2be147c50b14a538343b586e0d50997f6f3529da
Mark Rothermal <rothermal2@comcast.net>
2016-10-13 17:36:48 -0500

    fun with methods

--- src/hello/MethodsPartDeux.java
+++ src/hello/MethodsPartDeux.java
@@ -5,23 +5,25 @@ import java.util.Scanner;
 public class MethodsPartDeux {
 
 	public static void main(String args []){
-		
+		methodTwo(methodOne());
+		methodThree();
 
 	}
 	 public static String methodOne(){
-		String a = " Hello";
+		String a = "Hello,";
 		return a;
 	}
 	 public static void methodTwo(String greeting){
-		 String b = " How are you today";
+		 String b = "How are you today.";
 		 String c = greeting + " " + b;
+		 System.out.println(c);
 		  
 	 }
-	 public static String methodThree(String greeting){
+	 public static void  methodThree(){
+		 System.out.println("What is your name?");
 		 Scanner input = new Scanner(System.in);
 		 String a = input.next();
-		 String hello = a + greeting;
-		 Return hello;
+		 System.out.println("Nice to meet you, " + a );
 	 }
 	 public static void dennyninetySeven(){
 		 System.out.println("Denny 97 yall!!!");
--- src/hello/arrays.java
+++ src/hello/arrays.java
@@ -1,30 +1,28 @@
-package hello;
-
-import java.util.Scanner;
-
-public class arrays {
-
-	public static void main(String args []){
-		int [] numbers = new int [5];	
-		numbers[0] = 15;
-		numbers[1] = 225;
-		numbers[2] = 115;
-		numbers[3] = 15;
-		numbers[4] = 145;
-//		numbers[5] = this would error because you only declared an array with 5 spots
-		
-		int [] a = new int[10];
-		for(int i = 0; i<a.length; i++){
-			a[i]=i;	
-		}
-		Scanner scan = new Scanner(System.in);
-		int x = scan.nextInt();
-		int[] b = new int[x];
-		
-		char[] letters = {'a','b','c','d','e'};
-		// note char values are single quote, strings are double quote...
-		
-		
-		
-	}
-}
+//package hello;
+//
+//import java.util.Scanner;
+//
+//public class arrays {
+//
+//	public static void main(String args []){
+//		int [] numbers = new int [5];	
+//		numbers[0] = 15;
+//		numbers[1] = 225;
+//		numbers[2] = 115;
+//		numbers[3] = 15;
+//		numbers[4] = 145;
+////		numbers[5] = this would error because you only declared an array with 5 spots
+//		
+//		int [] a = new int[10];
+//		for(int i = 0; i<a.length; i++){
+//			a[i]=i;	
+//		}
+//		Scanner scan = new Scanner(System.in);
+//		int x = scan.nextInt();
+//		int[] b = new int[x];
+//		
+//		char[] letters = {'a','b','c','d','e'};
+//		// note char values are single quote, strings are double quote...
+//		
+//	}
+//}
