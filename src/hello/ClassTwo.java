cd0488b019735a1f3734b07639ba363f6b2de65b
Mark Rothermal <rothermal2@comcast.net>
2016-10-15 19:30:58 -0500

    importing classes

---
+++ src/hello/ClassTwo.java
@@ -0,0 +1,8 @@
+package hello;
+
+public class ClassTwo {
+	
+		public static void two (){
+			System.out.println("This is class two");		
+		}
+}
--- src/hello/MethodsPartDeux.java
+++ src/hello/MethodsPartDeux.java
@@ -1,32 +1,32 @@
-package hello;
-
-import java.util.Scanner;
-
-public class MethodsPartDeux {
-
-	public static void main(String args []){
-		methodTwo(methodOne());
-		methodThree();
-
-	}
-	 public static String methodOne(){
-		String a = "Hello,";
-		return a;
-	}
-	 public static void methodTwo(String greeting){
-		 String b = "How are you today.";
-		 String c = greeting + " " + b;
-		 System.out.println(c);
-		  
-	 }
-	 public static void  methodThree(){
-		 System.out.println("What is your name?");
-		 Scanner input = new Scanner(System.in);
-		 String a = input.next();
-		 System.out.println("Nice to meet you, " + a );
-	 }
-	 public static void dennyninetySeven(){
-		 System.out.println("Denny 97 yall!!!");
-		 System.out.println("Denny 97 again and again.");
-	 }
-}
+//package hello;
+//
+//import java.util.Scanner;
+//
+//public class MethodsPartDeux {
+//
+//	public static void main(String args []){
+//		methodTwo(methodOne());
+//		methodThree();
+//
+//	}
+//	 public static String methodOne(){
+//		String a = "Hello,";
+//		return a;
+//	}
+//	 public static void methodTwo(String greeting){
+//		 String b = "How are you today.";
+//		 String c = greeting + " " + b;
+//		 System.out.println(c);
+//		  
+//	 }
+//	 public static void  methodThree(){
+//		 System.out.println("What is your name?");
+//		 Scanner input = new Scanner(System.in);
+//		 String a = input.next();
+//		 System.out.println("Nice to meet you, " + a );
+//	 }
+//	 public static void dennyninetySeven(){
+//		 System.out.println("Denny 97 yall!!!");
+//		 System.out.println("Denny 97 again and again.");
+//	 }
+//}
