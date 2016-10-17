eedda145010ef422b923e6b87e1ce0f91761ae8a
Mark Rothermal <rothermal2@comcast.net>
2016-10-17 17:15:01 -0500

    working with packages

--- src/people/Person.java
+++ src/people/Person.java
@@ -1,5 +1,12 @@
 package people;
 
+	
 public class Person {
-
+	private int age;
+	private String name;
+	
+	public Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
 }
--- src/places/City.java
+++ src/places/City.java
@@ -1,5 +1,11 @@
 package places;
 
 public class City {
-
+	private String name;
+	private String location;
+	
+	public City(String name, String location){
+		this.name = name;
+		this.location = location;
+	}
 }
