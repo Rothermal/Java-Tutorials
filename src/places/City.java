ba563ffa50e916747a5c29543a88c69b23b69d35
Mark Rothermal <rothermal2@comcast.net>
2016-10-19 17:35:28 -0500

    importing packages

--- src/places/City.java
+++ src/places/City.java
@@ -8,4 +8,16 @@ public class City {
 		this.name = name;
 		this.location = location;
 	}
+	public String getName(){
+		return name;
+	}
+	public String getLocation(){
+		return location;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public void setLocation(String location){
+		this.location = location;
+	}
 }
--- src/things/item.java
+++
@@ -1,5 +0,0 @@
-package things;
-
-public class item {
-
-}
