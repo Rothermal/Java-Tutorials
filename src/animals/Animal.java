0e652675c99534620486d06c4190666e6737db2a
Mark Rothermal <rothermal2@comcast.net>
2016-10-21 17:08:17 -0500

    working with Inheratance

---
+++ src/animals/Animal.java
@@ -0,0 +1,16 @@
+package animals;
+// abstract classes cannot be made into objects. 
+// so if i want to make an animal with this  "superclass"
+// i will need to use inheritance and extend a separate class.
+
+public abstract class Animal {
+	public boolean isAPet = true;
+	public String owner ="Mark";
+	
+	public void sleep(){
+		System.out.println("Sleeping");
+	}
+	public void eat(){
+		System.out.println("Eating");
+	}
+}
---
+++ src/animals/Main.java
@@ -0,0 +1,8 @@
+package animals;
+
+public class Main {
+	public static void main(String args[]){
+		Dog d = new Dog();
+		d.sleep();
+	}
+}
