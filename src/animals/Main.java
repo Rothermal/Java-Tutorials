fafccb0f04ace337eaa42a794426c7b684021d28
Mark Rothermal <rothermal2@comcast.net>
2016-10-23 15:12:41 -0500

    jframes

--- src/animals/Main.java
+++ src/animals/Main.java
@@ -1,8 +1,8 @@
 package animals;
 
 public class Main {
-	public static void main(String args[]){
-		Dog d = new Dog();
-		d.sleep();
-	}
+//	public static void main(String args[]){
+//		Dog d = new Dog();
+//		d.sleep();
+//	}
 }
