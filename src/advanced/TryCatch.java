da2f2554e1ae534aba6c66cc195e2f5cefe91107
Mark Rothermal <rothermal2@comcast.net>
2016-10-26 17:38:43 -0500

    working on Jlabel and Jframe

--- src/advanced/TryCatch.java
+++ src/advanced/TryCatch.java
@@ -1,19 +1,21 @@
 package advanced;
 
 public class TryCatch {
-	public static void main(String args[]){
-		int num1, num2;
-		try{
-		num1 = 0;
-		num2 = 14 / num1;
-		System.out.println("Inside try block");
-		}
-		catch(ArithmeticException e){
-			// catch diving by 0 exception
-			System.out.println("Error: Dont Divide a number by 0");
-			
-			
-		}
-		System.out.println("outside of the catch block");
-	}
+//	public static void main(String args[]){
+//		int num1, num2;
+//		try{
+//		num1 = 0;
+//		num2 = 14 / num1;
+//		System.out.println("Inside try block");
+//		}
+//		catch(ArithmeticException e){
+//			// you don't always have to supply the exception type you can say
+//			// Exception e and that will catch all errors
+//			// catch diving by 0 exception
+//			System.out.println("Error: Dont Divide a number by 0");
+//			
+//			
+//		}
+//		System.out.println("outside of the catch block");
+//	}
 }
---
+++ src/jframe/MarkLand.java
@@ -0,0 +1,41 @@
+package jframe;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class MarkLand {
+	public static void main(String args []){
+	JFrame frame = new JFrame();
+	ImageIcon logoIcon = new ImageIcon("KappacatBlue.png");
+	Image logo = logoIcon.getImage();
+	frame.setSize(1200, 800);
+	frame.setTitle("MarkLand");
+	frame.setIconImage(logo);
+	frame.setResizable(false);
+	frame.setVisible(true);
+	JPanel panel = new JPanel();
+	frame.add(panel);
+//JLabel label= new JLabel("Hello",logo,JLabel.CENTER);
+	
+	
+	
+//	   JFrame frame = new JFrame();        
+//       String title = "Hello App";        
+//       frame.setTitle(title);        
+//       frame.setSize(300, 200);        
+//       frame.setDefaultCloseOperation        
+//           (JFrame.EXIT_ON_CLOSE);        
+//       frame.setVisible(true);        
+//       JPanel panle = new JPanel();        
+//       frame.add(panle);        
+//       JLabel lable = new JLabel("Hello World!!!");        
+//       panle.add(lable);  
+
+	
+
+	}
+}
